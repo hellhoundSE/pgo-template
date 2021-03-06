43730936b8e6b792d8f762386d9ffe80fe98e06a
valuievruslan <51120432+hellhoundSE@users.noreply.github.com>
2021-03-06 18:02:20 +0100

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -12,22 +12,23 @@ import java.io.*;
 public class TestHello {
 
     @Test
-    public void testTelephone() throws IOException {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
-        Telephone t = new Telephone("arra",12,"22222");
-        t.sendSMS("kuku","666666");
-
-        assertEquals("wysylam wiadomosc kuku na numer 666666\n".trim(), bos.toString().trim());
-        //assertEquals("wysylam wiadomosc kuku na numer 666666\n", "wysylam wiadomosc kuku na numer 666666\n");
-
-        // undo the binding in System
+    public void testProperty(){
+        String name = "Sony";
+        int res = 12;
+        String number = "123123";
+        Telephone t = new Telephone(name,res,number);
+        assertAll(
+                () ->
+                    assertEquals(name,t.model),
+                ()->
+                    assertEquals(res,t.resolution),
+                ()->
+                    assertEquals(number,t.number)
+                );
     }
 
     @Test
-    public void testTelephone2() {
+    public void testSendSMS() {
         PrintStream originalOut = System.out;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(bos));
@@ -36,8 +37,5 @@ public class TestHello {
         t.sendSMS("kuku","111111");
 
         assertEquals("wysylam wiadomosc kuku na numer 111111\n".trim(), bos.toString().trim());
-        //assertEquals("wysylam wiadomosc kuku na numer 666666\n", "wysylam wiadomosc kuku na numer 666666\n");
-
-        // undo the binding in System
     }
 }
