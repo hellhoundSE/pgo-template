01bfd12c8980b8172cfa843d01989a5545cd0ef0
valuievruslan <valuievruslan@gmail.com>
2021-03-09 01:41:54 +0100

    add tests

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -3,12 +3,11 @@ package com.example.project;
 public class Main {
 
 	public static void main(String[] args) {
-		printHelloWorld();
+
 	}
 
-	public static void printHelloWorld(){
-		System.out.println("Hello World");
-	}
+
+
 
 }
 
--- src/test/java/com/example/project/TestClassTemplate.java
+++ src/test/java/com/example/project/TestClassTemplate.java
@@ -9,11 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestClassTemplate {
 
-//    @Test
-//    public void testName() {
-//
-//
-//    }
+    @Test
+    public void testHelloWorld() {
+            Main main = GenericTestFactory.getInstance(Main.class);
+            GenericTestFactory.testSystemOutputFromMethod(main,"printHelloWorld","Hello World",null,null);
+    }
+
+    @Test
+    public void testMultiplyNumbers() {
+        int first = 5;
+        int second = 10;
+        Main main = GenericTestFactory.getInstance(Main.class);
+        GenericTestFactory.testMethod(main,"multiplyNumbers",50,new Class[]{int.class,int.class},new Object[]{first,second}, true);
+    }
 
 }
 
